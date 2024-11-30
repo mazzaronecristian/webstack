52419c9184f896d0547ceee3aba8624b8ade428b
cristian mazzarone <c.mazzarone@firlab.com>
2024-11-30 12:03:07 +0100

    primo commit

---
+++ php_code/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CTF SQL Injection</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form>
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
+
