c66bc1f51688034597e46edc29d6515daa0a423e
cristian mazzarone <c.mazzarone@firlab.com>
2024-12-09 23:21:36 +0100

    commit

--- php_code/header.php
+++ php_code/header.php
@@ -6,9 +6,6 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src='app.js'></script>
-
-<script ></script>
-
 </head>
